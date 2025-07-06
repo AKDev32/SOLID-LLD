6970e81e6143709cf53c8ec0362a567d24d65c50
AKDev32 <amankumar005562@gmail.com>
2025-07-06 12:29:35 +0530

    commit Lession1

---
+++ src/Lession1/Employee.java
@@ -0,0 +1,18 @@
+package Lession1;
+
+public class Employee {
+
+    public int id;
+
+    public int getId () {
+        return 0;
+    }
+
+    public String fetchBioData () {
+        return "Some Bio Data";
+    }
+
+    public void printPerformanceData () {
+        System.out.println("Some performance related data");
+    }
+}
