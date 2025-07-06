6799fa5223e7ee05b5aeacde8bfed150c8e00f0b
AKDev32 <amankumar005562@gmail.com>
2025-07-06 16:09:50 +0530

    commit config

---
+++ src/Lession1/Example4/BetterCode/LogUserActivity.java
@@ -0,0 +1,8 @@
+package Lession1.Example4.BetterCode;
+
+public class LogUserActivity {
+
+    public void logUserActivity (String username) {
+        // Code to log user's activity
+    }
+}
---
+++ src/Lession1/Example4/BetterCode/UserManager.java
@@ -0,0 +1,15 @@
+package Lession1.Example4.BetterCode;
+
+public class UserManager {
+
+//    userRepository userRepo;
+
+    public void createUser (String username) {
+        // Code to create a user
+
+    }
+
+    public void deleteUser (String username) {
+        // Code to delete the user
+    }
+}
